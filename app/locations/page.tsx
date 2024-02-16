22933a992ecdfbf2ebec9101e6a15ebfc7575395
Trend20 <enockomondi305@gmail.com>
2024-02-16 09:26:32 +0300

    adding locations component and locationCard component

---
+++ app/locations/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { getAllLocations } from "../api/locations";
+import LocationCard from "@/components/LocationCard";
+
+const Locations = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    page?: string;
+  };
+}) => {
+  const currentPage = searchParams?.page || "1";
+  const { info, results } = await getAllLocations(currentPage);
+  return (
+    <main className="flex flex-col w-full">
+      <h1 className="text-2xl font-bold">Locations</h1>
+      <div className="grid grid-cols-4 gap-10">
+        {results.map((location) => (
+          <LocationCard key={location.id} location={location} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default Locations;
---
+++ components/LocationCard.tsx
@@ -0,0 +1,22 @@
+import { Location } from "@/types/location";
+import Link from "next/link";
+
+export default function LocationCard({ location }: { location: Location }) {
+  return (
+    <div className="flex flex-col shadow-xl p-8 rounded-md w-full bg-slate-300">
+      <div className="flex items-center">
+        <h3 className="text-2xl font-bold mr-5">Name:</h3>
+        <Link
+          href={`/locations/${location.id}`}
+          className="flex text-sky-500 text-lg"
+        >
+          {location.name}
+        </Link>
+      </div>
+      <div className="mt-3">
+        <span className="font-bold text-2xl mr-5">Type:</span>
+        <span>{location.type}</span>
+      </div>
+    </div>
+  );
+}
