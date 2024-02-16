895612206efd11c28cd1e9ab8a00828d02c4bbbd
Trend20 <enockomondi305@gmail.com>
2024-02-16 10:06:02 +0300

    adding the characters api endpoint

---
+++ app/api/characters.ts
@@ -0,0 +1,48 @@
+// import { ApiResponse, Character } from "../../types/apiTypes";
+import { RICK_AND_MORTY_API_URL } from "@/config";
+import { ApiResponse } from "@/types/apiResponse";
+import { Character } from "@/types/character";
+
+export async function getAllCharacters(
+  currentPage: string
+): Promise<ApiResponse<Character[]>> {
+  const response = await fetch(
+    `${RICK_AND_MORTY_API_URL}/character/?page=${currentPage}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+
+  return response.json();
+}
+
+export async function getCharacterById(
+  characterId: string
+): Promise<Character> {
+  const response = await fetch(
+    `${RICK_AND_MORTY_API_URL}/character/${characterId}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data.");
+  }
+
+  return response.json();
+}
+
+export async function getCharactersFromUrls(
+  charactersUrls: string[]
+): Promise<Character[]> {
+  const characters = charactersUrls.map(async (url) => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data.");
+    }
+
+    return response.json();
+  });
+
+  return Promise.all(characters);
+}
